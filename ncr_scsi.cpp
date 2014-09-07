d484b8a2baaced83e5ace2f3662bd0f5376961c7
Toni Wilen <twilen@winuae.net>
2014-09-07 11:30:56 +0300

    NCR53C720+ CD led.

--- ncr_scsi.cpp
+++ ncr_scsi.cpp
@@ -159,6 +159,9 @@ int32_t scsi_req_enqueue(SCSIRequest *req)
 {
 	struct scsi_data *sd = (struct scsi_data*)req->dev->handle;
 
+	if (sd->device_type == UAEDEV_CD)
+		gui_flicker_led (LED_CD, sd->id, 1);
+
 	sd->data_len = 0;
 	scsi_start_transfer(sd);
 	scsi_emulate_analyze(sd);
