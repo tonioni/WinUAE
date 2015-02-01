92adb73689db734b5799251c7eec1825e06e18c2
Toni Wilen <twilen@winuae.net>
2015-02-01 16:51:20 +0200

    Clock sync accuracy update.

--- cia.cpp
+++ cia.cpp
@@ -604,7 +604,7 @@ static void do_tod_hack (int dotod)
 	}
 	if (rate <= 0)
 		return;
-	if (rate != oldrate || ciaatod != tod_hack_tod_last) {
+	if (rate != oldrate || (ciaatod & 0xfff) != (tod_hack_tod_last & 0xfff)) {
 		write_log (_T("TOD HACK reset %d,%d %d,%d\n"), rate, oldrate, ciaatod, tod_hack_tod_last);
 		tod_hack_reset ();
 		oldrate = rate;
