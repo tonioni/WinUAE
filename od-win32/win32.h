b769f5e5b97cfb4a7a0ced3e07c6a696dc5f1a6c
Toni Wilen <twilen@winuae.net>
2015-05-25 21:14:29 +0300

    3100b22

--- od-win32/win32.h
+++ od-win32/win32.h
@@ -20,12 +20,12 @@
 #define LANG_DLL_FULL_VERSION_MATCH 1
 
 #if WINUAEPUBLICBETA
-#define WINUAEBETA _T("21")
+#define WINUAEBETA _T("22")
 #else
 #define WINUAEBETA _T("")
 #endif
 
-#define WINUAEDATE MAKEBD(2015, 5, 23)
+#define WINUAEDATE MAKEBD(2015, 5, 25)
 
 //#define WINUAEEXTRA _T("AmiKit Preview")
 //#define WINUAEEXTRA _T("Amiga Forever Edition")
@@ -34,7 +34,7 @@
 #define WINUAEEXTRA _T("")
 #endif
 #ifndef WINUAEREV
-#define WINUAEREV _T("RC1")
+#define WINUAEREV _T("RC2")
 #endif
 
 #define IHF_WINDOWHIDDEN 6
