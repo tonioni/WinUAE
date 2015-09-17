82283caf1abaa884ad6e7e0658de862ff6306ec5
Frode Solheim <frode@fs-uae.net>
2015-09-17 22:02:56 +0200

    JIT: Re-enable direct memory access for _WIN64

--- jit/compemu.h
+++ jit/compemu.h
@@ -456,10 +456,7 @@ typedef struct {
 } regacc;
 
 #define JIT_EXCEPTION_HANDLER
-#ifdef _WIN64
-/* Direct addressing currently causes crash on 64-bit Windows. */
-#define JIT_ALWAYS_DISTRUST
-#endif
+// #define JIT_ALWAYS_DISTRUST
 
 /* ARAnyM uses fpu_register name, used in scratch_t */
 /* FIXME: check that no ARAnyM code assumes different floating point type */
