e5255e2733f6019335a3dee15b02aef1c65ba00f
Frode Solheim <frode@fs-uae.net>
2015-10-04 20:08:55 +0200

    move read_proceesor_time (and co) to support/time.cpp

--- od-win32/machdep/rpt.h
+++ od-win32/machdep/rpt.h
@@ -1,16 +1,1 @@
-/*
-  * UAE - The Un*x Amiga Emulator
-  *
-  * Definitions for accessing cycle counters on a given machine, if possible.
-  *
-  * Copyright 1997, 1998 Bernd Schmidt
-  * Copyright 1999 Brian King - Win32 specific
-  */
-#ifndef _RPT_H_
-#define _RPT_H_
-
-typedef unsigned long frame_time_t;
-extern frame_time_t read_processor_time (void);
-extern uae_u32 read_system_time (void);
-
-#endif
+#include "uae/time.h"
