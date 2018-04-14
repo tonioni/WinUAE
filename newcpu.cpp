2b7c4e9440cb593eb69d340488aa1520c2ebab43
Toni Wilen <twilen@winuae.net>
2018-04-14 10:27:21 +0300

    Increase PC if $4afc breakpoint.

--- newcpu.cpp
+++ newcpu.cpp
@@ -3977,8 +3977,10 @@ uae_u32 REGPARAM2 op_illg (uae_u32 opcode)
 		return 4;
 	}
 
-	if (debugmem_illg(opcode))
+	if (debugmem_illg(opcode)) {
+		m68k_incpc_normal(2);
 		return 4;
+	}
 
 	if (cloanto_rom && (opcode & 0xF100) == 0x7100) {
 		m68k_dreg (regs, (opcode >> 9) & 7) = (uae_s8)(opcode & 0xFF);
