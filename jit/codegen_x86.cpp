0939b8e7c15173ff0df502416e34c953e44d3fc9
Toni Wilen <twilen@winuae.net>
2018-05-21 22:30:38 +0300

    4000b11

--- jit/codegen_x86.cpp
+++ jit/codegen_x86.cpp
@@ -1309,7 +1309,7 @@ static inline void x86_fadd_m(MEMR s)
 	ADDR32 FADDLm(s,X86_NOREG,X86_NOREG,1);
 }
 
-#else
+#else /* !USE_NEW_RTASM */
 
 const bool optimize_accum	= true;
 const bool optimize_imm8	= true;
@@ -3098,7 +3098,7 @@ static inline void x86_fadd_m(MEMR s)
 	emit_long(s);
 }
 
-#endif
+#endif /* USE_NEW_RTASM */
 
 /*************************************************************************
  * Unoptimizable stuff --- jump                                          *
