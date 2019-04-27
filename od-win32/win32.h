e2e6c7dcb56f8dc2509d5792987a52f4fc40fa86
Toni Wilen <twilen@winuae.net>
2019-04-27 21:00:16 +0300

    4210b2

--- od-win32/win32.h
+++ od-win32/win32.h
@@ -20,12 +20,12 @@
 #define LANG_DLL_FULL_VERSION_MATCH 0
 
 #if WINUAEPUBLICBETA
-#define WINUAEBETA _T("1")
+#define WINUAEBETA _T("2")
 #else
 #define WINUAEBETA _T("")
 #endif
 
-#define WINUAEDATE MAKEBD(2019, 4, 18)
+#define WINUAEDATE MAKEBD(2019, 4, 27)
 
 //#define WINUAEEXTRA _T("AmiKit Preview")
 //#define WINUAEEXTRA _T("Amiga Forever Edition")
