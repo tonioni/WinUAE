834a3186046f9fc2aee2b00fd011c88e64cbb9bd
Toni Wilen <twilen@winuae.net>
2019-08-16 22:27:28 +0300

    Always clear extra parameters.

--- debug.cpp
+++ debug.cpp
@@ -5632,6 +5632,7 @@ static bool debug_line (TCHAR *input)
 		case 't':
 			no_trace_exceptions = 0;
 			debug_cycles();
+			trace_param1 = trace_param2 = 0;
 			if (*inptr == 't') {
 				no_trace_exceptions = 1;
 				inptr++;
