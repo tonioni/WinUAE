4e392f34711f84d9d98f9140dbaca2e10143db49
Toni Wilen <twilen@winuae.net>
2021-08-15 19:58:57 +0300

    4900b30

--- od-win32/win32.h
+++ od-win32/win32.h
@@ -20,12 +20,12 @@
 #define LANG_DLL_FULL_VERSION_MATCH 1
 
 #if WINUAEPUBLICBETA
-#define WINUAEBETA _T("Beta 29")
+#define WINUAEBETA _T("Beta 30")
 #else
 #define WINUAEBETA _T("")
 #endif
 
-#define WINUAEDATE MAKEBD(2021, 8, 9)
+#define WINUAEDATE MAKEBD(2021, 8, 15)
 
 //#define WINUAEEXTRA _T("AmiKit Preview")
 //#define WINUAEEXTRA _T("Amiga Forever Edition")
