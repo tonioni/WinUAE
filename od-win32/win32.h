2484348b05cd3c703b7503975487fbc9df40c2ca
Toni Wilen <twilen@winuae.net>
2022-01-22 16:38:53 +0200

    4910b6

--- od-win32/win32.h
+++ od-win32/win32.h
@@ -15,17 +15,17 @@
 #define GETBDM(x) (((x) - ((x / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define WINUAEPUBLICBETA 1
+#define WINUAEPUBLICBETA 0
 #define LANG_DLL 1
 #define LANG_DLL_FULL_VERSION_MATCH 0
 
 #if WINUAEPUBLICBETA
-#define WINUAEBETA _T("Beta 5")
+#define WINUAEBETA _T("Beta 6")
 #else
 #define WINUAEBETA _T("")
 #endif
 
-#define WINUAEDATE MAKEBD(2022, 1, 15)
+#define WINUAEDATE MAKEBD(2022, 1, 22)
 
 //#define WINUAEEXTRA _T("AmiKit Preview")
 //#define WINUAEEXTRA _T("Amiga Forever Edition")
