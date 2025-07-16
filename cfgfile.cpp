52caf4b456258ac9caede98197bb4b67394f7256
Frode Solheim <frode@fs-uae.net>
2025-07-16 14:12:35 +0200

    Fix ata_parse_identity declaration, use ide.h in cfgfile.cpp

--- cfgfile.cpp
+++ cfgfile.cpp
@@ -44,6 +44,7 @@
 #ifdef WITH_SPECIALMONITORS
 #include "specialmonitors.h"
 #endif
+#include "ide.h"
 
 #define cfgfile_warning write_log
 #define cfgfile_warning_obsolete write_log
@@ -5179,9 +5180,6 @@ static bool parse_geo (const TCHAR *tname, struct uaedev_config_info *uci, struc
 		xfree(out);
 		ret = true;
 	}
-
-	void ata_parse_identity(uae_u8 *out, struct uaedev_config_info *uci, bool *lba, bool *lba48, int *max_multiple);
-	bool ata_get_identity(struct ini_data *ini, uae_u8 *out, bool overwrite);
 
 	uae_u8 ident[512];
 	if (ata_get_identity(ini, ident, true)) {
