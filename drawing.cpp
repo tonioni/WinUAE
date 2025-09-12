3eca33a1c88368427f2946b8b60b03f63e5d174a
Toni Wilen <twilen@winuae.net>
2025-09-12 16:18:33 +0300

    Reset fast mode state when centering changes.

--- drawing.cpp
+++ drawing.cpp
@@ -5491,6 +5491,7 @@ static void draw_denise_vsync(int erase)
 		// delay until next frame to prevent single black frame
 		erase_next_draw = true;
 		center_y_erase = false;
+		resetfulllinestate();
 	}
 }
 
