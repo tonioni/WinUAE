bfb832e700c0927a9894deb1ce6489e8e6b44a13
Toni Wilen <twilen@winuae.net>
2025-10-11 18:21:57 +0300

    6020b2

--- od-win32/win32.h
+++ od-win32/win32.h
@@ -20,12 +20,12 @@
 #define LANG_DLL_FULL_VERSION_MATCH 1
 
 #if WINUAEPUBLICBETA
-#define WINUAEBETA _T("1")
+#define WINUAEBETA _T("2")
 #else
 #define WINUAEBETA _T("")
 #endif
 
-#define WINUAEDATE MAKEBD(2025, 9, 28)
+#define WINUAEDATE MAKEBD(2025, 10, 11)
 
 //#define WINUAEEXTRA _T("AmiKit Preview")
 //#define WINUAEEXTRA _T("Amiga Forever Edition")
