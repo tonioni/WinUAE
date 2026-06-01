0ae0cc7401d8385284f2b66750af96a606d34b5b
Dimitris Panokostas <midwan@gmail.com>
2026-06-01 08:46:07 +0200

    fix: preserve DIVU flags in noflags JIT path

--- jit/arm/compemu_support_arm.cpp
+++ jit/arm/compemu_support_arm.cpp
@@ -3701,8 +3701,10 @@ void build_comp(void)
         }
         prop[cft_map(opcode)].set_flags = table68k[opcode].flagdead;
         prop[cft_map(opcode)].use_flags = table68k[opcode].flaglive;
-        if (table68k[opcode].mnemo == i_DIVU)
+        if (table68k[opcode].mnemo == i_DIVU) {
             prop[cft_map(opcode)].use_flags |= FLAG_CZNV;
+            nfcompfunctbl[cft_map(opcode)] = compfunctbl[cft_map(opcode)];
+        }
         /* Unconditional jumps don't evaluate condition codes, so they
          * don't actually use any flags themselves */
         if (prop[cft_map(opcode)].cflow & fl_const_jump)
