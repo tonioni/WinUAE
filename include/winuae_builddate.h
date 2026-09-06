ccc407e9c636668b99b5f19d6e9aa03ea04b3052
Toni Wilen <twilen@winuae.net>
2026-09-06 19:04:02 +0300

    6100b13

--- include/winuae_builddate.h
+++ include/winuae_builddate.h
@@ -6,6 +6,6 @@
 #define GETBDM(x) (((x) - ((x / 10000) * 10000)) / 100)
 #define GETBDD(x) ((x) % 100)
 
-#define WINUAEDATE MAKEBD(2026, 8, 16)
+#define WINUAEDATE MAKEBD(2026, 9, 6)
 
 #endif
--- od-win32/win32.h
+++ od-win32/win32.h
@@ -17,7 +17,7 @@
 #define LANG_DLL_FULL_VERSION_MATCH 1
 
 #if WINUAEPUBLICBETA
-#define WINUAEBETA _T("12")
+#define WINUAEBETA _T("13")
 #else
 #define WINUAEBETA _T("")
 #endif
